839570fce88019869dc9e10911701dc947df26e2
Angela Villota <angievig@gmail.com>
2019-02-15 17:57:51 +0100

    updated jar

--- com.coffee.compiler/src/com/coffee/compiler/Compiler.java
+++ com.coffee.compiler/src/com/coffee/compiler/Compiler.java
@@ -3,10 +3,25 @@ package com.coffee.compiler;
 import java.util.Properties;
 
 public class Compiler implements ICompiler{
+	
+
+	
+	public Compiler(Properties prop, String model) {
+	
+		
+	}
 
 	@Override
 	public void compileMinizinc(String modelPath, Properties operations) {
 		// TODO Auto-generated method stub
+		
+	}
+	/**
+	 * Method that compiles a variability model regarding the type of problem 
+	 * @param properties properties file with the dialect and the operations
+	 * @param model variability model as a minizinc model
+	 */
+	public static void compile(Properties properties, String model) {
 		
 	}
 
